aae58639d8bd242e68438f4ca436aabc82c57655
Matt Newcombe <matt.newcombe@unity3d.com>
2023-04-16 19:13:15 +0700

    [UI] Experimentally switching to UI Toolkit

---
+++ toy_unity/Assets/_uiToolKit/SetVersionInfoLabel.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.Utilities;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class SetVersionInfoLabel : MonoBehaviour
+{
+    public bool DisplayVersion = true;
+    public bool DisplayBuildNumber;
+    public bool DisplayBuildTime = true;
+    public bool DisplayBuildMachineName;
+    public bool DisplayGitCommitShortHash = true;
+
+    private UIDocument _document;
+    private Label _versionInfoLabel;
+
+    private void Start()
+    {
+        _document = GetComponent<UIDocument>();
+        _versionInfoLabel = _document.rootVisualElement.Q<Label>("VersionInfo");
+        _versionInfoLabel.text  = ConcatInfo();
+    }
+
+    private string ConcatInfo()
+    {
+        string info = "";
+        
+        if (DisplayVersion) AddArgument(ref info, VersionInfo.Asset.version);
+        if (DisplayBuildNumber) AddArgument(ref info, VersionInfo.Asset.buildNumber);
+        if (DisplayBuildTime) AddArgument(ref info, VersionInfo.Asset.buildTime);
+        if (DisplayBuildMachineName) AddArgument(ref info, VersionInfo.Asset.buildMachineName);
+        if (DisplayGitCommitShortHash) AddArgument(ref info, VersionInfo.Asset.gitCommitShortHash);
+
+        return info;
+    }
+
+    private void AddArgument(ref string info, string argument)
+    {
+        info += info.IsNullOrWhitespace() ? argument : " | " + argument;
+    }
+}
